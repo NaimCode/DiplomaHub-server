ee71eb8e3ef00ff7becc6d9fa0141c0c445eb23c
Mahamat Naim Abdelkerim <73735806+NaimCode@users.noreply.github.com>
2022-04-10 12:48:57 +0000

    email d'inscription 80%

--- routes/inscription.js
+++ routes/inscription.js
@@ -1,15 +1,28 @@
 const router = require("express").Router();
+const hbs = require("nodemailer-express-handlebars");
+const path = require("path");
+
 const transporter = require("../config/nodemailer.js");
+const handlebarOptions = {
+  viewEngine: {
+    partialsDir: path.resolve("views/"),
+    defaultLayout: false,
+  },
+  viewPath: path.resolve("views/"),
+};
+transporter.use("compile", hbs(handlebarOptions));
 router.post("/", (req, res) => {
+  console.log("inscription...");
   var mailOptions = {
     from: process.env.EMAIL,
     to: process.env.EMAIL_ADMINS,
     subject: "Demande d'inscription",
-    html: "<h1>Naim</h1>",
+    template: "demandeInscription",
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
+      console.log(error);
       res.status(500).send({ message: "Erreur du server" });
     } else {
       res.status(200).send({
