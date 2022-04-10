558d68c9ab57434607317bee522dbb6f1f5cb198
Mahamat Naim Abdelkerim <73735806+NaimCode@users.noreply.github.com>
2022-04-10 16:24:34 +0000

    confirmation 0%

--- routes/inscription.js
+++ routes/inscription.js
@@ -3,6 +3,7 @@ const hbs = require("nodemailer-express-handlebars");
 const path = require("path");
 
 const transporter = require("../config/nodemailer.js");
+const getDateTime = require("../function/datetime.js")();
 const handlebarOptions = {
   viewEngine: {
     partialsDir: path.resolve("views/"),
@@ -18,6 +19,21 @@ router.post("/", (req, res) => {
     to: process.env.EMAIL_ADMINS,
     subject: "Demande d'inscription",
     template: "demandeInscription",
+    context: {
+      abrev: req.body.abrev ?? "---",
+      nom: req.body.nom ?? "---",
+      email: req.body.email ?? "---",
+      num: req.body.num ?? "---",
+      site: req.body.site ?? "---",
+      adresse: req.body.adresse ?? "---",
+      pays: req.body.pays ?? "---",
+      ville: req.body.ville ?? "---",
+      frontendUrl: process.env.FRONTEND_URL ?? "http://localhost:3000",
+      confirmationUrl:
+        (process.env.BACKEND_URL ?? "http://localhost:4000") +
+        "/inscription/confirmation",
+      date: getDateTime,
+    },
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
